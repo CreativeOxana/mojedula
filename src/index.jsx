f3a7f7dbde6d8c0d33f7750ffba06f9e013935a5
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-24 16:32:36 +0200

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from 'react-dom/client';
+import './global.css';
+import { HomePage } from './pages/HomePage';
+
+createRoot(document.querySelector('#app')).render(<HomePage />);
