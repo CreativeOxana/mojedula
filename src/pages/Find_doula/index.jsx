4726e001d07e2acf715654efd6984c8c46089afb
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-06-10 19:05:58 +0200

    update

--- src/pages/Find_doula/index.jsx
+++ src/pages/Find_doula/index.jsx
@@ -1,20 +1,35 @@
+import Map from '../components/Map/Map';
+import DulaList from '../components/DulaList/DulaList';
+
+import { useState } from 'react';
+
 export const Find_doula = () => {
-  const fetchDuly = async () => {
-    const response = await fetch(`localhost:4000/api/duly`);
-    const json = await response.json();
-    const duly = json.data;
-  };
-  fetchDuly();
+  const [regionId, setRegionId] = useState('');
+
+  function handleDataFromChild(data) {
+    setRegionId(data);
+  }
+  // const fetchDuly = async () => {
+  //   const response = await fetch(`localhost:4000/api/duly`);
+  //   const json = await response.json();
+  //   const duly = json.data;
+  // };
+  // fetchDuly();
+
   return (
     <>
-      {response.map((item) => (
+      <div>
+        <Map sendDataToParent={handleDataFromChild} />
+        <DulaList region={regionId} />
+      </div>
+      {/* {response.map((item) => (
         <>
           <p key={item.id}>{item.jméno}</p>
           <p>{item.region}</p>
           <p>{item.akreditace}</p>
           <p>{item.jazyk}</p>
         </>
-      ))}
+      ))} */}
     </>
   );
 };
--- src/pages/components/DulaList/DulaList.jsx
+++ src/pages/components/DulaList/DulaList.jsx
@@ -7,51 +7,71 @@ export default function ({ region }) {
       id: 1,
       name: 'Berková Lenka, Mgr.',
       regionIds: ['cz-pr', 'cz-st'],
+      accretitaion: 'dula absolventka',
+      language: ['český', 'francouzský'],
     },
     {
       id: 2,
       name: 'Broncová Kateřina',
       regionIds: ['cz-pr', 'cz-pa', 'cz-kr'],
+      accretitaion: 'certifikovaná dula',
+      language: ['český'],
     },
     {
       id: 3,
       name: 'Hájková Kateřina',
       regionIds: ['cz-pr', 'cz-st'],
+      accretitaion: 'dula absolventka',
+      language: ['český', 'anglický'],
     },
     {
       id: 4,
       name: 'Hrabincová Irena, PhDr.',
       regionIds: ['cz-li', 'cz-kr'],
+      accretitaion: 'certifikovaná dula, konzultantka',
+      language: ['český'],
     },
     {
       id: 5,
       name: 'Lamošová Veronika, Ing.',
       regionIds: ['cz-pr', 'cz-st'],
+      accretitaion: 'certifikovaná dula',
+      language: ['český'],
     },
     {
       id: 6,
       name: 'Mašková Pavla, Ing.',
       regionIds: ['cz-jc'],
+      accretitaion: 'dula studentka',
+      language: ['český'],
     },
     {
       id: 7,
       name: 'Nerodilová Petra, Ing.',
       regionIds: ['cz-pr', 'cz-st'],
+      accretitaion: 'dula absolventka',
+      language: ['český', 'anglický', 'španělský'],
     },
     {
       id: 8,
       name: 'Remundová Ivona, Bc.',
       regionIds: ['cz-pr'],
+      accretitaion: 'certifikovaná dula',
+      language: ['český'],
     },
     {
       id: 9,
       name: 'Sokolová Zuzana, DiS.',
       regionIds: ['cz-pr', 'cz-st', 'cz-vy'],
+      accretitaion: 'certifikovaná dula',
+      language: ['český', 'anglický'],
     },
     {
       id: 10,
       name: 'Zámečníková Hana, Mgr.',
       regionIds: ['cz-ol'],
+      accretitaion: 'certifikovaná dula',
+      language: ['český'],
     },
   ];
 
@@ -62,7 +82,13 @@ export default function ({ region }) {
     <>
       <ul>
         {currentDulas.map((cd, index) => (
-          <li key={index}>{cd.name}</li>
+          <>
+            <ul>
+              <li key={index}>
+                {cd.name}, {cd.accretitaion}, {cd.language}
+              </li>
+            </ul>
+          </>
         ))}
       </ul>
     </>
