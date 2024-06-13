8dd60aa862ee58e41c9606be358e8cd5b9f8d0c6
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-06-13 23:04:46 +0200

    bigger map

--- src/pages/Contacts/index.jsx
+++ src/pages/Contacts/index.jsx
@@ -9,13 +9,13 @@ export const Contacts = () => {
         <strong>Kde nás najdete? </strong> Badeniho 1, Praha 6, 160 00
       </p>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2559.4254400640607!2d14.404977276935456!3d50.09704331274636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470b94df6a700001%3A0x7530a92d540647b!2sBadeniho%20290%2F1%2C%20160%2000%20Praha%206-Letn%C3%A1!5e0!3m2!1scs!2scz!4v1717938138099!5m2!1scs!2scz"
-        width="450"
-        height="350"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2559.4254439855504!2d14.402681294756313!3d50.097043239337275!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x470b94df6a700001%3A0x7530a92d540647b!2sBadeniho%20290%2F1%2C%20160%2000%20Praha%206-Letn%C3%A1!5e0!3m2!1scs!2scz!4v1718312627448!5m2!1scs!2scz"
+        width="800"
+        height="600"
         style={{ border: 0 }}
-        allowFullScreen=""
+        allowfullscreen=""
         loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
+        referrerpolicy="no-referrer-when-downgrade"
       ></iframe>
       <i class="fa-solid fa-envelope"></i>
       <p>
