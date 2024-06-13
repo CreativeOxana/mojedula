e05feb96d399be17231e6c7cf8768dcb3390ad5c
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-06-13 21:24:23 +0200

    bigger icons added

--- src/pages/components/Footer/index.jsx
+++ src/pages/components/Footer/index.jsx
@@ -5,18 +5,18 @@ export const Footer = () => {
     <>
       <footer>
         <a href="https://www.facebook.com/ceske.duly/?locale=cs_CZ">
-          <i className="fa-brands fa-facebook"></i>
+          <i className="fa-brands fa-facebook fa-xl"></i>
         </a>
         <a href="https://www.facebook.com/ceske.duly/?locale=cs_CZ">
           <p>České duly</p>{' '}
         </a>
         <a href="https://www.instagram.com/ceske.duly/">
-          <i className="fa-brands fa-instagram"></i>
+          <i className="fa-brands fa-instagram fa-xl"></i>
         </a>
         <a href="https://www.instagram.com/ceske.duly/">
           <p>ceske.duly</p>
         </a>
-        <i class="fa-solid fa-envelope"></i>
+        <i class="fa-solid fa-envelope fa-xl"></i>
         <p>
           <a href="mailto: info@mojedula.cz">info@mojedula.cz</a>
         </p>
