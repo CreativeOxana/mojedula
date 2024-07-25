9d58d42aeccc5b0db2996471124e0e2ff13b5b3e
Oxana Chiziţcaia <110328307+CreativeOxana@users.noreply.github.com>
2024-07-25 11:24:09 +0200

    Update index.jsx

--- src/pages/components/About/index.jsx
+++ src/pages/components/About/index.jsx
@@ -1,6 +1,6 @@
 export const About = () => {
   return (
-    <div className="container about">
+    <div className="container info">
       <div className="contact">
         <p>Badeniho 1, Praha 6, 160 00</p>
         <p>IČO: 26559366</p>
