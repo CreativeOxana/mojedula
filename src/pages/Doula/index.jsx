803fe8e4b88f833d3b0ddb070f7e555779e95934
Oxana Chiziţcaia <110328307+CreativeOxana@users.noreply.github.com>
2024-07-25 11:23:33 +0200

    Update index.jsx

--- src/pages/Doula/index.jsx
+++ src/pages/Doula/index.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const Doula = () => {
   return (
     <>
-      <div className="container dula">
+      <div className="container about">
         <h1>Kdo je dula</h1>
         <div className="about-box">
           <img
