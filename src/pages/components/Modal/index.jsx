d7302d1a1123d4c150f90fe73ed42380566488d9
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-09-29 14:51:40 +0200

    update

--- src/pages/components/Modal/index.jsx
+++ src/pages/components/Modal/index.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import DulaList from '../DulaList/DulaList';
 
 export default function Modal() {
   const [isModalOpen, setIsModalOpen] = useState(false);
