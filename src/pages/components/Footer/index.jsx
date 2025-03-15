1dc5e2b5c10b22e1fb5fad075eb03f74e86c5eae
Oxana Chiziţcaia <creativesouloxana@gmail.com>
2025-03-15 20:53:59 +0100

    oprava aria label footer

--- src/pages/components/Footer/index.jsx
+++ src/pages/components/Footer/index.jsx
@@ -8,20 +8,29 @@ export const Footer = () => {
           <a
             href="https://www.facebook.com/ceske.duly/?locale=cs_CZ"
             target="_blank"
+            aria-label="Sledujte nás na Facebooku"
           >
-            <i className="fa-brands fa-facebook fa-xl"></i>
+            <i className="fa-brands fa-facebook fa-xl" aria-hidden="true"></i>
           </a>
 
-          <a href="https://www.instagram.com/ceske.duly/" target="_blank">
-            <i className="fa-brands fa-instagram fa-xl"></i>
+          <a
+            href="https://www.instagram.com/ceske.duly/"
+            target="_blank"
+            aria-label="Sledujte nás na Instagramu"
+          >
+            <i className="fa-brands fa-instagram fa-xl" aria-hidden="true"></i>
           </a>
 
-          <a href="mailto: info@mojedula.cz" target="_blank">
-            <i className="fa-solid fa-envelope fa-xl"></i>
+          <a
+            href="mailto: info@mojedula.cz"
+            target="_blank"
+            aria-label="Napište nám e-mail"
+          >
+            <i className="fa-solid fa-envelope fa-xl" aria-hidden="true"></i>
           </a>
         </div>
         <div className="copyright">
-          <i className="fa-regular fa-copyright"></i> 2024
+          <i className="fa-regular fa-copyright"></i> 2025
         </div>
       </footer>
     </>
