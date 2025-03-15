9c13047ae48c5d22690b4b9131fc709d9b45a816
Oxana Chiziţcaia <creativesouloxana@gmail.com>
2025-03-15 19:52:14 +0100

    correct DulaList

--- src/pages/components/DulaList/DulaList.jsx
+++ src/pages/components/DulaList/DulaList.jsx
@@ -10,7 +10,7 @@ export default function DulaList({ region }) {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fecthData = async () => {
+    const fetchData = async () => {
       setLoading(true);
       try {
         const response = await fetch('/assets/api/duly.json');
@@ -26,42 +26,47 @@ export default function DulaList({ region }) {
       }
     };
 
-    fecthData();
-  }, []);
+    fetchData();
+  }, [region]);
+
+  // const handleDulaClick = (dula) => {
+  //   if (selectedDula && selectedDula.id === dula.id) {
+  //     setShowModal(false);
+  //     setSelectedDula(null);
+  //   } else {
+  //     setSelectedDula(dula);
+  //     setShowModal(true);
+  //   }
 
   const handleDulaClick = (dula) => {
-    if (selectedDula && selectedDula.id === dula.id) {
-      setShowModal(false);
-      setSelectedDula(null);
-    } else {
-      setSelectedDula(dula);
-      setShowModal(true);
-    }
-
-    const currentDulas =
-      region === ''
-        ? dulas
-        : dulas.filter((dula) => dula.regionIds.includes(region));
-
-    if (loading) return <p>Načítání...</p>;
-    if (error) return <p>Chyba při načítání: {error}</p>;
-
-    return (
-      <>
-        <ul className="container dula box">
-          {currentDulas.map((dula) => (
-            <li key={dula.id} onClick={() => handleDulaClick(dula)}>
-              {dula.name}, {dula.accreditation}, jazyky:{' '}
-              {dula.language.join(', ')}
-            </li>
-          ))}
-        </ul>
-        {showModal && selectedDula && (
-          <>
-            <Modal dula={selectedDula} onClose={() => setShowModal(false)} />
-          </>
-        )}
-      </>
-    );
+    setSelectedDula(selectedDula && selectedDula.id === dula.id ? null : dula);
+    setShowModal(!showModal);
   };
+
+  const currentDulas =
+    region === ''
+      ? dulas
+      : dulas.filter((dula) => dula.regionIds.includes(region));
+
+  if (loading) return <p>Načítání...</p>;
+  if (error) return <p>Chyba při načítání: {error}</p>;
+
+  return (
+    <>
+      <ul className="container dula box">
+        {currentDulas.map((dula) => (
+          <li key={dula.id} onClick={() => handleDulaClick(dula)}>
+            {dula.name}, {dula.accreditation}, jazyky:{' '}
+            {dula.language.join(', ')}
+          </li>
+        ))}
+      </ul>
+      {showModal && selectedDula && (
+        <>
+          <Modal dula={selectedDula} onClose={() => setShowModal(false)} />
+        </>
+      )}
+    </>
+  );
 }
+// }
