36c7b2b1684a332cf90c86efc3a9d52cb015a6ca
Oxana Chiziţcaia <creativesouloxana@gmail.com>
2026-01-11 13:54:09 +0100

    update Modal style plus forwardRef

--- src/index.jsx
+++ src/index.jsx
@@ -10,42 +10,50 @@ import { Contacts } from './pages/Contacts';
 import { App } from './pages/components/App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: '/',
+          element: <HomePage />,
+        },
+        {
+          path: '/doula',
+          element: <Doula />,
+        },
+        {
+          path: '/about',
+          element: <About />,
+        },
+        {
+          path: '/education',
+          element: <Education />,
+        },
+        {
+          path: '/events',
+          element: <Events />,
+        },
+        {
+          path: '/find_doula',
+          element: <Find_doula />,
+        },
+        {
+          path: '/contacts',
+          element: <Contacts />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path: '/doula',
-        element: <Doula />,
-      },
-      {
-        path: '/about',
-        element: <About />,
-      },
-      {
-        path: '/education',
-        element: <Education />,
-      },
-      {
-        path: '/events',
-        element: <Events />,
-      },
-      {
-        path: '/find_doula',
-        element: <Find_doula />,
-      },
-      {
-        path: '/contacts',
-        element: <Contacts />,
-      },
-    ],
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    },
   },
-]);
+);
 
 createRoot(document.querySelector('#app')).render(
   <RouterProvider router={router} />,
--- src/pages/components/Modal/index.jsx
+++ src/pages/components/Modal/index.jsx
@@ -1,42 +1,45 @@
 import React from 'react';
+import { forwardRef } from 'react';
 import './modal.css';
 import { regionNames } from '../../../utils/regionNames';
 
-export default function Modal({ dula, onClose }) {
+const Modal = forwardRef(({ dula, onClose }, ref) => {
   return (
-    <>
-      <div className="modal-overlay">
-        <div className="modal">
-          <button className="modal-close" onClick={onClose}>
-            &times;
-          </button>
-          <h2>Medailonek vybrané duly</h2>
-          <div>
-            <h3>{dula.name}</h3>
-            <img src={dula.picture} alt={dula.name} />
-            <p>
-              <b>Akreditace:</b> {dula.accreditation}
-            </p>
-            <p>
-              <b>Jazyky: </b> {dula.language.join(', ')}
-            </p>
-            <p>
-              <b>Region působnosti: </b>{' '}
-              {dula.regionIds && dula.regionIds.length > 0
-                ? dula.regionIds
-                    .map((regionId) => regionNames[regionId] || regionId)
-                    .join(', ')
-                : 'Region není uveden'}
-            </p>
-            <p>
-              <b>Webové stránky: </b>
-              {dula.website}
-            </p>
-          </div>
-          <div dangerouslySetInnerHTML={{ __html: dula.medallion }}></div>
-          <button onClick={onClose}>Zavřít</button>
+    <div className="modal-overlay">
+      <div className="modal" ref={ref}>
+        <button className="modal-close" onClick={onClose}>
+          &times;
+        </button>
+        <h2>Medailonek vybrané duly</h2>
+        <div>
+          <h3>{dula.name}</h3>
+          <img src={dula.picture} alt={dula.name} />
+          <p>
+            <b>Akreditace:</b> {dula.accreditation}
+          </p>
+          <p>
+            <b>Jazyky: </b> {dula.language.join(', ')}
+          </p>
+          <p>
+            <b>Region působnosti: </b>{' '}
+            {dula.regionIds && dula.regionIds.length > 0
+              ? dula.regionIds
+                  .map((regionId) => regionNames[regionId] || regionId)
+                  .join(', ')
+              : 'Region není uveden'}
+          </p>
+          <p>
+            <b>Webové stránky: </b>
+            {dula.website}
+          </p>
         </div>
+        <div dangerouslySetInnerHTML={{ __html: dula.medallion }}></div>
+        <button onClick={onClose}>Zavřít</button>
       </div>
-    </>
+    </div>
   );
-}
+});
+
+Modal.displayName = 'Modal';
+
+export default Modal;
